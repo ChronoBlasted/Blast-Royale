eb6035778a76bb43f61b3eb93b28c4de721acb8e
ChronoBlasted <74500014+ChronoBlasted@users.noreply.github.com>
2025-02-10 16:51:32 +0100

    feat: create Item data Ref assets

--- Assets/Scripts/Class/SO/ItemDataRef.cs
+++ Assets/Scripts/Class/SO/ItemDataRef.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewNakamaDataRef", menuName = "ScriptableObjects/NakamaDataRef/NewNakamaDataRef", order = 0)]
+[CreateAssetMenu(fileName = "NewItemDataRef", menuName = "ScriptableObjects/NakamaDataRef/NewItemDataRef", order = 0)]
 public class ItemDataRef : NakamaDataRef
 {
 }
