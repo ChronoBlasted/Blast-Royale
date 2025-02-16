4d0a89eee9a23de1e4594e9fa15ec51430e77429
ChronoBlasted <74500014+ChronoBlasted@users.noreply.github.com>
2025-02-16 15:55:28 +0100

    feat: create move data ref

--- Assets/Scripts/Class/SO/ItemDataRef.cs
+++ Assets/Scripts/Class/SO/ItemDataRef.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewItemDataRef", menuName = "ScriptableObjects/NakamaDataRef/NewItemDataRef", order = 0)]
+[CreateAssetMenu(fileName = "NewItemDataRef", menuName = "ScriptableObjects/NakamaDataRef/NewItemDataRef", order = 1)]
 public class ItemDataRef : NakamaDataRef
 {
 }
--- Assets/Scripts/Class/SO/MoveDataRef.cs
+++ Assets/Scripts/Class/SO/MoveDataRef.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "NewMoveDataRef", menuName = "ScriptableObjects/NakamaDataRef/NewMoveDataRef", order = 2)]
 public class MoveDataRef : NakamaDataRef
 {
 
