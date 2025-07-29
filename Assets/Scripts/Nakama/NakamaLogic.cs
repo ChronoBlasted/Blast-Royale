059841901a91326004ad9260d299160e558847e9
ChronoBlasted <74500014+ChronoBlasted@users.noreply.github.com>
2025-07-29 15:31:51 +0200

    Fix: setup server IP

--- Assets/Scripts/Nakama/NakamaLogic.cs
+++ Assets/Scripts/Nakama/NakamaLogic.cs
@@ -1,9 +1,7 @@
 using BaseTemplate.Behaviours;
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Localization.Plugins.XLIFF.V12;
 using UnityEngine;
 
 public class NakamaLogic : MonoSingleton<NakamaLogic>
